701a9e75c126f91fd630d96342a80891c2c8ea0f
ashwinpra <ashprasanth2003@gmail.com>
2023-10-16 20:37:27 +0530

    done?

--- server/app.py
+++ server/app.py
@@ -5,6 +5,7 @@ import requests
 import sqlite3
 import os
 from dotenv import load_dotenv
+import json
 
 load_dotenv()
 KEY = os.getenv('NEWSAPI_API_KEY')
@@ -14,27 +15,31 @@ CORS(app)
 
 # Initialize the SQLite database
 db = sqlite3.connect('news.db')
-db.execute('CREATE TABLE IF NOT EXISTS news (title TEXT, description TEXT, imgSrc TEXT, source TEXT, url TEXT)')
+db.execute('CREATE TABLE IF NOT EXISTS news (title TEXT, description TEXT, publishedAt TIMESTAMP, imgSrc TEXT, source TEXT, url TEXT, country TEXT)')
 db.commit()
 db.close()
 
+with open('../src/countries.json') as f:
+    countries = json.load(f)
+    
 @app.route('/fetch-news', methods=['GET'])
 def fetch_news():
     try:
-        # Fetch news articles from NewsAPI
-        url = 'https://newsapi.org/v2/top-headlines?country=in&apiKey={}'
-        response = requests.get(url.format(KEY))
-        articles = response.json().get('articles', [])
-
         # Store articles in the database
         db = sqlite3.connect('news.db')
         db.execute('CREATE UNIQUE INDEX IF NOT EXISTS title ON news (title)')
         cursor = db.cursor()
-        for article in articles: 
-            try: 
-                cursor.execute('INSERT INTO news (title, description, imgSrc, source, url) VALUES (?, ?, ?, ?, ?)', (article['title'], article['description'], article['urlToImage'], article['source']['name'], article['url']))
-            except sqlite3.IntegrityError:
-                pass
+
+        # Fetch news articles from NewsAPI
+        url = 'https://newsapi.org/v2/top-headlines?country={}&apiKey={}'
+
+        # fetch news for each country
+        for country in countries:
+            response = requests.get(url.format(country, KEY))
+            print(response.json())
+            articles = response.json()['articles']
+            cursor.executemany('INSERT OR IGNORE INTO news VALUES (?, ?, ?, ?, ?, ?, ?)', [(article['title'], article['description'], article['publishedAt'], article['urlToImage'], article['source']['name'], article['url'], country) for article in articles])
+
         db.commit()
         db.close()
 
@@ -51,7 +56,8 @@ def get_news():
         cursor = db.cursor()
         cursor.execute('SELECT * FROM news')
         # fetch as a list of dictionaries
-        news = [{'title': row[0], 'description': row[1], 'imgSrc': row[2], 'source': row[3], 'url': row[4]} for row in cursor.fetchall()]
+        news = [{'title': row[0], 'description': row[1], 'publishedAt': row[2], 'imgSrc': row[3], 'source': row[4], 'url': row[5], 'country': row[6]} for row in cursor.fetchall()]
+        news.sort(key=lambda x: x['publishedAt'], reverse=True)
         db.close()
         return jsonify(news)
     except Exception as e:
